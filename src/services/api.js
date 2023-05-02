689b6d399714271e86d77ab26d3196fe8eaedb4a
Adzz <73648780+63070047@users.noreply.github.com>
2023-05-02 20:03:43 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://44.212.2.30:5001/api/",
+  baseURL: "http://backend-1819812.us-east-1.elb.amazonaws.com/api",
 });
 
 export default api;
