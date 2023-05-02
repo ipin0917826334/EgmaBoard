01e41fdf00a74c6c4d2282704e678bda6d8507cd
ipin0917826334 <71114672+ipin0917826334@users.noreply.github.com>
2023-05-03 04:08:43 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,17 +5,8 @@ const topicsRouter = require('./routes/topics');
 
 const app = express();
 app.use(express.json());
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (origin === 'http://localhost:3000') {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use('/api/topics', topicsRouter);
 
 mongoose.connect('mongodb://63070049:Asd04140@ac-hf8gfdn-shard-00-00.rliflax.mongodb.net:27017,ac-hf8gfdn-shard-00-01.rliflax.mongodb.net:27017,ac-hf8gfdn-shard-00-02.rliflax.mongodb.net:27017/?ssl=true&replicaSet=atlas-va9ist-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
